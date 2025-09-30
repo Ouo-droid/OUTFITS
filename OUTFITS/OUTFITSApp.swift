6f6d037d1cfbe2fd2362b5b37f46091a77f8a901
A G <lounabrisebois@gmail.com>
2025-09-30 20:09:00 +0200

    Initial Commit

---
+++ OUTFITS/OUTFITSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OUTFITSApp.swift
+//  OUTFITS
+//
+//  Created by Antoine  Gallo on 30/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct OUTFITSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OUTFITSTests/OUTFITSTests.swift
@@ -0,0 +1,16 @@
+//
+//  OUTFITSTests.swift
+//  OUTFITSTests
+//
+//  Created by Antoine  Gallo on 30/09/2025.
+//
+
+import Testing
+
+struct OUTFITSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
